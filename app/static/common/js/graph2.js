37f103b88dae6d553201a2d48e6d7055de607722
juiceyang <abandonclock@gmail.com>
2016-02-21 01:39:40 +0800

    add cursors class group cursors to a group

--- app/static/common/js/graph2.js
+++ app/static/common/js/graph2.js
@@ -1,24 +1,22 @@
-function LiveLineGraph() {
+function Cursors(svg, wps) {
     var self = this;
-    self.svg = null;
-    self.data_set = [];
-    self.timespan = 600;
+    self.parent_svg = svg;
     self.cursor_x = [];
     self.cursor_count = 10;
-    self.w = 800;
-    self.h = 500;
-    self.wps = self.w / self.timespan;
-    self.container_id = "live-graph-div";
-
+    self.cursor_svg_margin = 30;
+    self.wps = wps;
     self.init = function () {
-        self.svg = d3.select("#" + self.container_id)
-            .append("svg")
-            .attr("width", self.w)
-            .attr("height", self.h);
-        for(var i=0; i<self.cursor_count; i++){
-            self.cursor_x[i]=self.w/self.cursor_count*i;
+        for (var i = 0; i < self.cursor_count; i++) {
+            self.cursor_x[i] = self.parent_svg.attr("width") / self.cursor_count * i;
         }
-        self.svg.selectAll("line")
+        var cursor_svg = self.parent_svg.append("svg")
+            .attr("x", self.cursor_svg_margin)
+            .attr("y", self.cursor_svg_margin)
+            .attr("width", self.parent_svg.attr("width") - 2 * self.cursor_svg_margin)
+            .attr("height", self.parent_svg.attr("height") - 2 * self.cursor_svg_margin);
+        var cursor_g = cursor_svg.append("g")
+            .attr("class", "cursor-g");
+        cursor_g.selectAll("line")
             .data(self.cursor_x)
             .enter()
             .append("line")
@@ -29,24 +27,19 @@ function LiveLineGraph() {
                 return d;
             })
             .attr("y1", 0)
-            .attr("y2", self.h)
+            .attr("y2", cursor_svg.attr("height"))
             .attr("stroke", "gray")
             .attr("stroke-width", 1);
     };
-    self.update_cursor_x = function(){
-        for (var i=0; i<self.cursor_x.length; i++){
-            if(self.cursor_x[i]<0){
-                self.cursor_x[i]+=self.w;
-            }
-        }
-    };
-    self.render_graph = function () {
-        // render cursor lines
+    self.update = function () {
         for (var i = 0; i < self.cursor_x.length; i++) {
             self.cursor_x[i] -= self.wps;
-        }
-        self.update_cursor_x();
-        self.svg.selectAll("line")
+            if (self.cursor_x[i] < 0) {
+                self.cursor_x[i] += self.parent_svg.attr("width");
+            }
+            console.log(self.cursor_x);
+            self.parent_svg.select("g")
+                .selectAll("line")
             .data(self.cursor_x)
             .attr("x1", function (d, i) {
                 return d;
@@ -54,6 +47,36 @@ function LiveLineGraph() {
             .attr("x2", function (d, i) {
                 return d;
             });
+        }
+    };
+}
+
+
+function LiveLineGraph() {
+    var self = this;
+    self.svg = null;
+    self.cursors = null;
+    self.data_set = [];
+    self.timespan = 600;
+    self.w = 800;
+    self.h = 500;
+    self.wps = self.w / self.timespan;
+    self.container_id = "live-graph-div";
+
+    self.init = function () {
+        self.svg = d3.select("#" + self.container_id)
+            .append("svg")
+            .attr("width", self.w)
+            .attr("height", self.h);
+        self.cursors = new Cursors(self.svg, self.wps);
+        self.cursors.init();
+        // TODO: init scaled axis
+        // TODO: load data from api
+        // TODO: draw line
+    };
+    self.render_graph = function () {
+        // render cursor lines
+        self.cursors.update();
     };
 }
 
