cb6371cd66223f905ae6253558684019b91130d5
juiceyang <abandonclock@gmail.com>
2016-03-29 09:36:35 +0800

    implement sign in and get token

--- app/static/common/js/button.js
+++ app/static/common/js/button.js
@@ -2,22 +2,23 @@ function register_signin_btn() {
     $("#signin-submit-btn").click(function (event) {
         var un = $("#inputUsername").val();
         var pwd = $("#inputPassword").val();
-        var basicAuthorization = btoa(un + ':' + pwd);
         event.preventDefault();
         $.ajax({
-            type: "GET",
+            type: "POST",
             url: "http://localhost:5000/authenticate",
-            headers: {
-                "Authorization": "Basic " + basicAuthorization
+            headers:{
+                "Content-Type": "application/json"
             },
             dataType: 'json',
+            data: JSON.stringify({
+                "un": un,
+                "pwd": pwd
+            }),
             complete: function (data) {
                 if (data["responseJSON"]["err"] == "False") {
                     alert("sign in success!");
                     window.location.href = "/";
-                    // TODO: let's save pwd in cookies and get it run first
-                    Cookies.set("un", un);
-                    Cookies.set("pwd", pwd);
+                    Cookies.set("token", data.responseJSON.result.token);
                 }
             }
         });
